64035770952b596185caefea8e43b4673b3e9ec4
MitchelBroekate <mitchelb11@outlook.com>
2025-02-26 15:43:22 +0100

    progress customer behavior
    added more to the customer behavior

--- Don Squeakalini's Pizza/Assets/Scripts/Interactables/CutsomerInteraction.cs
+++ Don Squeakalini's Pizza/Assets/Scripts/Interactables/CutsomerInteraction.cs
@@ -5,6 +5,48 @@ using UnityEngine;
 public class CustomerInteraction : MonoBehaviour
 {
     //customer talks (pop-up)
+    Transform canvas;
+    List<GameObject> foodIcons = new();
+
+    CanvasGroup canvasAlpha;
+
+    void Start()
+    {
+        canvas = transform.GetChild(0).transform;
+
+        for(int i = 0; i < canvas.childCount; i++)
+        {
+            foodIcons.Add(canvas.GetChild(i).gameObject);
+        }
+    }
+
+    public IEnumerator ImageFade()
+    {
+        int imageCount = 0;
+
+        foreach(GameObject foodImage in foodIcons)
+        {
+            bool alphaIncrease = true;
+
+            SpriteRenderer currentSprite = foodIcons[imageCount].GetComponent<SpriteRenderer>();
+            canvasAlpha = foodIcons[imageCount].GetComponent<CanvasGroup>();
+
+            currentSprite.enabled = true;
+
+            while(alphaIncrease)
+            {
+                //canvasAlpha.alpha += 0.3 * Time.deltaTime;
+            }
+
+            yield return new WaitForSeconds(0.9f);
+
+            alphaIncrease = false;
+
+            imageCount++;
+        }
+        
+
+    }
 
     //icon fade-in
 }
---
+++ Don Squeakalini's Pizza/Assets/Scripts/Pathin/CustomerWalkBehavior.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerWalkBehavior : MonoBehaviour
+{
+    Transform checkpointParent;
+    [SerializeField] List<Transform> checkpoints = new();
+    [SerializeField] int currentCheckpoint;
+    bool customerWait = false;
+    bool reachedGround = false;
+    Rigidbody rb;
+
+    Quaternion targetRotation;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] float movementSpeed;
+
+    [SerializeField] CustomerInteraction customerInteraction;
+
+    void Start()
+    {
+        AddCheckpoints();
+        rb = GetComponent<Rigidbody>();
+        customerInteraction = GetComponent<CustomerInteraction>();
+    }
+
+    void Update()
+    {
+        if(!customerWait)
+        {
+            GoToCheckpoint();
+        }
+
+        if(!reachedGround)
+        {
+            rb.velocity = -transform.up * movementSpeed * Time.deltaTime; 
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            reachedGround = true;
+        }
+    }
+
+    void AddCheckpoints()
+    {
+        checkpointParent = GameObject.Find("WalkCheckpoints").transform;
+
+        for (int i = 0; i < checkpointParent.childCount; i++)
+        {
+            checkpoints.Add(checkpointParent.GetChild(i).transform);
+        }
+
+        currentCheckpoint = 0;
+    }
+
+    void GoToCheckpoint()
+    {
+        targetRotation = Quaternion.LookRotation(new Vector3(checkpoints[currentCheckpoint].transform.position.x, transform.position.y, checkpoints[currentCheckpoint].transform.position.z) - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+        //walk
+        if(reachedGround)
+        {
+            rb.velocity = transform.forward * movementSpeed * Time.deltaTime;   
+        }
+
+        if(Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(checkpoints[currentCheckpoint].position.x, checkpoints[currentCheckpoint].position.z)) < 1)
+        {
+            switch(currentCheckpoint)
+            {
+                case 0:
+                    currentCheckpoint++;
+                    break;
+                case 1:
+                    customerWait = true;
+                    rb.velocity = Vector3.zero;
+
+                    StartCoroutine(customerInteraction.ImageFade());
+                    break;
+                case 2:
+                    //walk to waiting area
+                    break;
+                case 3:
+                    //walk away
+                    break;
+                default:
+                    Debug.LogWarning("Int out of switch bounds (CustomerBehavior/GoToCheckpoint)");
+                    break;
+            } 
+        }
+
+    }
+}
