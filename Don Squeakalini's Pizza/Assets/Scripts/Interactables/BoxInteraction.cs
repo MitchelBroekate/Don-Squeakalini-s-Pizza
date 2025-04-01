e47fe58becd0d11fe50265aca456b278f87852e9
MitchelBroekate <mitchelb11@outlook.com>
2025-04-01 16:21:18 +0200

    pizza box fix

--- Don Squeakalini's Pizza/Assets/Scripts/Interactables/BoxInteraction.cs
+++ Don Squeakalini's Pizza/Assets/Scripts/Interactables/BoxInteraction.cs
@@ -5,15 +5,15 @@ public class BoxInteraction : MonoBehaviour
     [SerializeField] ObjectiveManager objectiveManager;
     [SerializeField] PlayerInteraction playerInteraction;
     [SerializeField] Transform itemHolder;
+    [SerializeField] GameObject pizzaBox;
     public void BoxInteract()
     {
         if(objectiveManager.OvenMinigameCompleted)
         {
             Destroy(itemHolder.GetChild(0).gameObject);
             objectiveManager.PizzaCompleet = true;
-            GameObject currentObject = Instantiate(gameObject, itemHolder.transform.position, Quaternion.identity);
+            GameObject currentObject = Instantiate(pizzaBox, itemHolder.transform.position, Quaternion.identity);
             currentObject.transform.parent = itemHolder;
-            currentObject.GetComponent<BoxCollider>().enabled = false;
             currentObject.layer = LayerMask.NameToLayer("Food");
         }
         else if(objectiveManager.PizzaCompleet)
